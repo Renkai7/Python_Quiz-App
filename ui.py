0942c64e52a75e6d1a5f1242c5dd4cd9623e9fcc
Mike <ms53150@gmail.com>
2022-05-15 12:00:00 -0400

    Add method to buttons for True or False answers

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,6 +19,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Sample",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -23,11 +27,25 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_button_img = PhotoImage(file="images/true.png")
-        self.true_btn = Button(image=true_button_img, highlightthickness=0)
+        self.true_btn = Button(image=true_button_img, highlightthickness=0, command=self.true_chosen)
         self.true_btn.grid(row=2, column=0)
 
         false_button_img = PhotoImage(file="images/false.png")
-        self.false_btn = Button(image=false_button_img, highlightthickness=0)
+        self.false_btn = Button(image=false_button_img, highlightthickness=0, command=self.false_chosen)
         self.false_btn.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_chosen(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+
+    def false_chosen(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()
